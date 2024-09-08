21c30da0c343078dccea09a5cee31ad94b8acc0f
hsnaregia <139281016+hsnaregia@users.noreply.github.com>
2024-09-09 01:08:52 +0330

    adding the api calls done styling part of the code for makling questionairs done

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const url = "https://opentdb.com/api.php?amount=10";
 const box = document.querySelectorAll('.boxes');
+const first = document.querySelector('.container');
+const second = document.querySelector('.question_cont');
+let que = document.getElementById('question');
+let an1= document.getElementById('answ1');
+let an2= document.getElementById('answ2');
+let an3= document.getElementById('answ3');
+let an4= document.getElementById('answ4')
 
+let questions = [];
+let right_answ = [];
+let wrng_answr=[];
 box_choice();
 // from 9 to 32;
-
-
 async function fetch_dat(api_url){
+  
   const response = await fetch(api_url);
   const data = await response.json();
   console.log(data);
+  for(let index = 0 ; index<10 ; index++){
+    
+    questions[index]=data.results[index].question;
+    
+    
+    right_answ[index]=data.results[index].correct_answer;
+    
+    
+    wrng_answr[index]=data.results[index].incorrect_answers;
+  }
+  console.log(questions);
+  console.log(right_answ);
+  console.log(wrng_answr);
+  
+  question_maker();
+  
 }
-
 
 
 function box_choice(){
@@ -20,7 +44,12 @@ function box_choice(){
       let info = ID(this.id);
       url_code =`${url}&category=${info}&type=multiple`;
       console.log(url_code);
-      // fetch_data(url_code);
+      setTimeout(() => {
+        first.classList.add('hide');
+        first.classList.remove('show');
+        second.classList.add('show');
+        second.classList.remove('hide')
+      }, 1000);
       fetch_dat(url_code);  
     })
   });    
@@ -33,3 +62,7 @@ function ID(data){
   return x;
 }  
 
+
+function question_maker(){
+  
+}
