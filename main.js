6f8b47bba32dcd129f20b946851ccc9bc46897eb
hsnaregia <139281016+hsnaregia@users.noreply.github.com>
2024-09-08 18:04:08 +0330

    after adding the assets for the boxes and styling them and making the api easily accesible the question parts stoll needed to be done in next changes

--- main.js
+++ main.js
@@ -4,23 +4,11 @@ const box = document.querySelectorAll('.boxes');
 box_choice();
 // from 9 to 32;
 
-function fetch_data(api_url) {
-  fetch(api_url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.json(); 
-    })
-    .then(data => {
-      console.log('Trivia Questions:', data.results);
-      data.results.forEach((question, index) => {
-        console.log(`${index + 1}. ${question.question} (Category: ${question.category})`);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching the trivia questions:', error);
-    });
+
+async function fetch_dat(api_url){
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log(data);
 }
 
 
@@ -30,10 +18,11 @@ function box_choice(){
   boxes.forEach((box) => {
     box.addEventListener('click' , function(){
       let info = ID(this.id);
-      url_code =`${url}&category=${info}`;
+      url_code =`${url}&category=${info}&type=multiple`;
       console.log(url_code);
-      fetch_data(url_code);
-      })
+      // fetch_data(url_code);
+      fetch_dat(url_code);  
+    })
   });    
   
 }  
@@ -43,5 +32,4 @@ function ID(data){
   x=parseInt(x)+8;
   return x;
 }  
-
 
