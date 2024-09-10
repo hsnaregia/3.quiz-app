726e02a3466e4fbcbc5898f1d58ec88494cd58c1
hsnaregia <139281016+hsnaregia@users.noreply.github.com>
2024-09-10 19:21:32 +0330

    working with where to put the answer randomaticly

--- main.js
+++ main.js
@@ -2,41 +2,68 @@ const url = "https://opentdb.com/api.php?amount=10";
 const box = document.querySelectorAll(".boxes");
 const first = document.querySelector(".container");
 const second = document.querySelector(".question_cont");
-
+let used_Nums=[];
 const question = [];
 let current = 0;
 box_choice();
 // from 9 to 32;
+
+
 async function fetch_dat(api_url) {
   const response = await fetch(api_url);
   const data = await response.json();
   console.log(data);
   for (let index = 0; index < 10; index++) {
     question[index] = {
-      id: index + 1,
+      id: (index + 1),
       quest: data.results[index].question,
-      answer: data.results[index].correct_answer,
+      answer: data.results[index].correct_answer+','+data.results[index].incorrect_answers,
       wrong: data.results[index].incorrect_answers,
     };
   }
-  console.log(question[2].id);
-  console.log(question[2].quest);
-  console.log(question[2].wrong[1]);
-
-  // showQuestion();
+  let x = 2;
+  showQuestion(x+1);
 }
 
-// function rando_ans(){
+function rand_generator(maxNr)
+{
+  let random = (Math.random() * maxNr).toFixed();
 
-// }
-// function showQuestion(){
-//   const questi = question[index];
-//   let que = document.getElementById("question").textContent = questi.quest;
-//   let an1 = document.getElementById("answ1");
-//   let an2 = document.getElementById("answ2");
-//   let an3 = document.getElementById("answ3");
-//   let an4 = document.getElementById("answ4");
-// }
+  //Coerce to number by boxing
+  random = Number(random)+1;
+
+  if(!used_Nums.includes(random)) {
+      used_Nums.push(random);
+      return random;
+  } else {
+      if(used_Nums < maxNr) {
+        //Recursively generate number
+       return  rand_generator(maxNr);
+      } else {
+        console.log('No more numbers available.')
+        return false;
+      }
+  }
+}
+
+
+function showQuestion(questionNumber){
+  const questi = question;
+  document.getElementById("quiz").innerText = questi[questionNumber].quest;
+  
+ for(let index=0 ; index <4 ; index++)
+  {
+  
+  rand_generator(4);
+  console.log(used_Nums);
+  document.getElementById(`answ${used_Nums[index]}`).textContent= questi[questionNumber].answer;
+
+ }
+
+}
+
+
+
 function box_choice() {
   const boxes = document.querySelectorAll(".boxes");
   boxes.forEach((box) => {
